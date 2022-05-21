7406b2b0d45975926d086d6515aa03ea5e4abcf0
Pietro Rhyan <pietroqjg@gmail.com>
2022-05-21 16:36:56 -0300

    Adjusting the route system

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
 
 export function App() {
   return (
-    <h1>Olá</h1>
+    <Routes>
+      <Route path="/" element={ <Home /> } />
+    </Routes>
   )
 }
 
